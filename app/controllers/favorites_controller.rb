6d504a03cf644681fb3cf9137f2fb32d40c06d33
EC2 Default User <ec2-user@ip-172-31-18-95.ap-northeast-1.compute.internal>
2024-08-21 08:40:05 +0000

    finish to create good button

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,15 +2,17 @@ class FavoritesController < ApplicationController
     
     def create
         book = Book.find(params[:book_id])
-        favorite = current_user.favorites.new(book_id: book.id)
-        favorite.save
-        redirect_to books_path
+        if book.user.id != current_user.id
+            favorite = current_user.favorites.new(book_id: book.id)
+            favorite.save
+        end
+        redirect_back fallback_location:root_path
     end
     
     def destroy
         book = Book.find(params[:book_id])
         favorite = current_user.favorites.find_by(book_id: book.id)
         favorite.destroy
-        redirect_to books_path
+        redirect_back fallback_location:root_path
     end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -5,4 +5,8 @@ class Book < ApplicationRecord
     
     validates :title, presence: true
     validates :body, length: { maximum: 200 }, presence: true
+    
+    def favorited_by?(user)
+        favorites.exists?(user_id: user.id)
+    end
 end
