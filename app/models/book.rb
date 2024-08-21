23632502e63336b79637a3102574e19bc2a3791d
EC2 Default User <ec2-user@ip-172-31-18-95.ap-northeast-1.compute.internal>
2024-08-21 07:52:49 +0000

    relationships between Favorite and User and Book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
     
     belongs_to :user
+    has_many :favorites, dependent: :destroy
     
     validates :title, presence: true
     validates :body, length: { maximum: 200 }, presence: true
